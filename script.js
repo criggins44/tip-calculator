87f05073e7fbdf82f0d68a3f2546f48af01a2d56
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-23 15:45:57 -0400

    added function to calculate the tip total and grand total

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //Tip Calculator
+
+function findTip(){
+	let bill = mealForm.bill.value;
+	let percentage = mealForm.tipPercent.value;
+	const tipAmount = document.getElementById('tipAmount');
+	const totalAmount = document.getElementById('totalAmount');
+	let convert = Number(percentage)/100;
+
+	let tipDollars = Number(bill) * convert;
+	let grandTotal = Number(bill) + tipDollars;
+
+	tipAmount.innerHTML = 'Tip Amount: $' + tipDollars;
+	totalAmount.innerHTML = 'Total Amount: $' + grandTotal;
+}
